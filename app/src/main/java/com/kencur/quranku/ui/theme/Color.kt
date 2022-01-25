944281dbee6477a81ec734f65d0d2dd19393609a
Julio Rahman <julio.rahman@gmail.com>
2022-01-25 10:43:03 +0700

    style: add assets
    - define some color
    - change some shape

--- app/src/main/java/com/kencur/quranku/ui/theme/Color.kt
+++ app/src/main/java/com/kencur/quranku/ui/theme/Color.kt
@@ -6,3 +6,9 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+val ColorPrimary = Color(0xFF29A19C)
+val ColorSecondary = Color(0xFF3DAEAA)
+val LabelPrimary = Color(0xFF0C1517)
+val LabelSecondary = Color(0xFF8A8A8E)
+val ColorBackgound = Color.White
